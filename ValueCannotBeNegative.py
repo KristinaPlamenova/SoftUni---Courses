162cc2fbbabeea3df1b1cd34de709dea64ab886c
Kristina Plamenova Krastinova <124092244+KristinaPlamenova@users.noreply.github.com>
2024-03-19 13:51:11 +0200

    Add files via upload

---
+++ ValueCannotBeNegative.py
@@ -0,0 +1,25 @@
+class ValueCannotBeNegative(Exception):
+    pass
+
+
+for _ in range(5):
+    number = int(input())
+    if number < 0:
+        raise ValueCannotBeNegative
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
